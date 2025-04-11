21721f60b0fc45ab75a1cbef5f7b4c764a37c792
Youssef <youssefyaser2020@gmail.com>
2025-04-11 04:40:24 +0200

    Handled main.js file

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
+import router from './index.js';
+import store from '../public/composables/CRUD_Operations.js';
 
-createApp(App).mount('#app')
+createApp(App).use(store).use(router).mount('#app');
